c8d7794d081c2ba6b3e9a98345ff9456bf5cf228
ernesthwang <github-ernesthwang@cf9.org>
2012-09-27 22:21:48 -0700

    * Adding a comment header to the stored procedure

--- RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
+++ RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -5,8 +6,20 @@ GO
 
 CREATE
 PROCEDURE
-	dbo.MyProcedure1
+	[dbo].[MyProcedure1]
 AS
+/*
+--------------------------------------------------
+Name:		[dbo].[MyProcedure1]
+Created:	2012.09.24
+Author:		Ernest Hwang
+Purpose:	Just for kicks!
+--------------------------------------------------
+
+	EXEC
+		dbo.MyProcedure1
+
+*/
 BEGIN
 	PRINT 'hello world'
 END
