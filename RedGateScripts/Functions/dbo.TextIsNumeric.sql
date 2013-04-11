b01e9bdc14623d2e2eea2c16690dbf55ee29b613
ernesthwang <github-ernesthwang@cf9.org>
2013-04-10 20:11:02 -0700

    Simplified the TextIsNumeric function

--- RedGateScripts/Functions/dbo.TextIsNumeric.sql
+++ RedGateScripts/Functions/dbo.TextIsNumeric.sql
@@ -31,17 +31,17 @@ History:
 
 
 	SELECT
-		dbo.[TextIsNumeric]('0')				AS	ShouldBe1
+		dbo.[TextIsNumeric]('0')			AS	ShouldBe1
 	,	dbo.[TextIsNumeric]('1234')			AS	ShouldBe1
 	,	dbo.[TextIsNumeric]('1234.23')			AS	ShouldBe1
 	,	dbo.[TextIsNumeric]('10.4E22')			AS	ShouldBe1
 
 	,	dbo.[TextIsNumeric]('<1234.23')			AS	ShouldBe0
 	,	dbo.[TextIsNumeric]('<1234')			AS	ShouldBe0
-	,	dbo.[TextIsNumeric](NULL)				AS	ShouldBe0
-	,	dbo.[TextIsNumeric]('.')				AS	ShouldBe0
-	,	dbo.[TextIsNumeric](' ')				AS	ShouldBe0
-	,	dbo.[TextIsNumeric]('ERNESTO IS 10.4E22')		AS	ShouldBe0
+	,	dbo.[TextIsNumeric](NULL)			AS	ShouldBe0
+	,	dbo.[TextIsNumeric]('.')			AS	ShouldBe0
+	,	dbo.[TextIsNumeric](' ')			AS	ShouldBe0
+	,	dbo.[TextIsNumeric]('ERNESTO IS 10.4E22')	AS	ShouldBe0
 
 	Alternate (non working 
 
@@ -72,20 +72,7 @@ History:
 
 */
 
-	DECLARE	@returnValue	INT
-	
-	IF CHARINDEX('E', UPPER(@TextValue))<> 0
-	BEGIN
-		-- if there is 'E' in the value - do a standard IsNumeric test
-		SET @returnValue = IsNumeric(@TextValue)
-	END
-	ELSE
-	BEGIN
-		-- Otherwise do a modified IsNumeric test by adding 'E0' at the end
-		SET @returnValue = IsNumeric(@TextValue + 'E0')
-	END
-
-	RETURN @returnValue
+	RETURN ISNUMERIC(@TextValue)
 
 END
 
