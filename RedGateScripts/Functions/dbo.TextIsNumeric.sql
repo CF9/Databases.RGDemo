0f2649b80b8ae309a2ff30325f7c5f9960bce4ad
ernesthwang <github-ernesthwang@cf9.org>
2013-10-09 00:01:17 -0700

    * Trying to "streamline" a user defined function

--- RedGateScripts/Functions/dbo.TextIsNumeric.sql
+++ RedGateScripts/Functions/dbo.TextIsNumeric.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -71,21 +72,7 @@ History:
 
 */
 
-	DECLARE	@returnValue	INT
-	
-	IF CHARINDEX('E', UPPER(@TextValue))<> 0
-	BEGIN
-		-- if there is 'E' in the value - do a standard IsNumeric test
-		SET @returnValue = IsNumeric(@TextValue)
-	END
-	ELSE
-	BEGIN
-		-- Otherwise do a modified IsNumeric test by adding 'E0' at the end
-		SET @returnValue = IsNumeric(@TextValue + 'E0')
-	END
-	
-	RETURN @returnValue
-
+	RETURN ISNUMERIC(@TextValue)
 
 END
 
