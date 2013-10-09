c07b2ac84996000e63ee839a52ac6aaa45cf64ac
ernesthwang <github-ernesthwang@cf9.org>
2013-10-09 14:05:20 -0700

    * Reverting the repository to the pre-demo state

--- RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
+++ RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
@@ -21,6 +21,6 @@ Purpose:	Just for kicks!
 
 */
 BEGIN
-	PRINT 'Hello, SQL in the City Users in Pasadena!!  GO DODGERS!!!'
+	PRINT 'Hello, SQL in the City Users in Pasadena!!  Vin Scully for President!!!'
 END
 GO
