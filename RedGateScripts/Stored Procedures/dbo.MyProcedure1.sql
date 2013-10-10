812dcb34d6c1e9b6e81bd96ac7409e0fce74b12c
ernesthwang <github-ernesthwang@cf9.org>
2013-10-10 14:37:41 -0700

    * I was more specific on when Vin Scully should become president

--- RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
+++ RedGateScripts/Stored Procedures/dbo.MyProcedure1.sql
@@ -21,6 +21,6 @@ Purpose:	Just for kicks!
 
 */
 BEGIN
-	PRINT 'Hello, SQL in the City Users in Pasadena!!  Vin Scully for President!!!'
+	PRINT 'Hello, SQL in the City Users in Pasadena!!  Vin Scully for President 2016!!!'
 END
 GO
